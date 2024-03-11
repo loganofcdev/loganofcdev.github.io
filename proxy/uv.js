b9712767d4078be911c4c3b567cbea9d9b3b2eef
loganofc <160552291+loganofcdev@users.noreply.github.com>
2024-03-11 15:58:32 -0500

    Delete proxy directory

--- proxy/uv.js
+++
@@ -1,7 +0,0 @@
-importScripts('/proxy/uv/uv.bundle.js');
-importScripts('/proxy/uv/uv.config.js');
-importScripts('/proxy/uv/uv.sw.js');
-
-const sw = new UVServiceWorker();
-
-self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
