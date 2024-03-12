e8c29cda1686215c4bfd516e16bda0c6de5859b2
loganofc <160552291+loganofcdev@users.noreply.github.com>
2024-03-11 22:15:06 -0500

    Update panic-key.js

--- extra/panic-key.js
+++ extra/panic-key.js
@@ -8,7 +8,7 @@
                 if (nextIndex === sequence.length) {
                     // Panic sequence detected
                     // Perform your panic action here
-                    window.location.href = 'classroom.google.com/'; // Change 'safe-page.html' to your safe page URL
+                    window.location.href = 'https://classroom.google.com/'; // Change 'safe-page.html' to your safe page URL
                 }
             } else {
                 // Reset sequence if the key pressed is not part of the sequence
